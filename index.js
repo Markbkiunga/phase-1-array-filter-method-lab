df2767b9924f82c23139ad67c2b5e865c5ea1847
Mark Brian <markbkiunga2017@gmail.com>
2024-07-02 11:28:02 +0300

    Completes Lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+function findMatching(driversArray, driver) {
+  return driversArray.filter((name) => {
+    return name.toLowerCase() === driver.toLowerCase();
+  });
+}
+// console.log(findMatching(drivers, "sally"));
+
+function fuzzyMatch(driversArray, driverString) {
+  return driversArray.filter((name) => {
+    return name.toLowerCase().startsWith(driverString.toLowerCase());
+  });
+}
+// console.log(fuzzyMatch(drivers, "y"));
+
+const driversHometowns = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+
+function matchName(driversObjectsInArray, driverString) {
+  return driversObjectsInArray.filter((driver) => {
+    return driver.name.toLowerCase() === driverString.toLowerCase();
+  });
+}
+// console.log(matchName(driversHometowns, "Bobby"));
